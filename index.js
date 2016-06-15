5ea72b1a703343ea6a70586e6ad491fc6ebd0473
mxprice <mxprice@godaddy.com>
2016-06-15 15:11:52 -0700

    Added kubectl -s <server> get <val> functionality

--- index.js
+++ index.js
@@ -2,21 +2,18 @@
 const program = require('commander');
 
 const handler = require('./lib/command_handlers');
-console.log("linked");
+
 program
 	.version('0.0.1')
 	.command('get <getValue>')
 	.option('-s, --server <server>', 'The server address and port')
-	.action(function(getValue, options) {
-		// console.log(getValue);
-		// console.log(serverAddress);
+	.parse(process.argv);
 
-		const serverAddress = options.server || undefined;
-		var options = {};
-		if (serverAddress != undefined)
-			options = {'-s': serverAddress};
+const serverAddress = program.server || undefined;
+var options = {};
+if (serverAddress != undefined)
+	options = {'-s': serverAddress};
 
-		handler.handleCommand('get', getValue, options);
-	});
+handler.handleCommand('get', program.getValue, options);
 
-program.parse(process.argv);
+// program.parse(process.argv);
